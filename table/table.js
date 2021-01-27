1db41ffc8347be083db05ff6a77605036dc041de
JinOhKwon <kjo1333@gmail.com>
2021-01-28 00:58:57 +0900

    기본 crud 완료

--- table/table.js
+++ table/table.js
@@ -14,20 +14,15 @@ let popUp = () => {
 
 class Table {
     container = document.getElementById("container");
-
     tableRef = document.createElement("table");
-    thRef = document.createElement("th");
-    tdRef = document.createElement("td");
-    trRef = this.tableRef.insertRow(-1);
 
     boards = [];
     cols = [];
     categorys = [];
+    data;
 
     constructor(data) {
-        this.boards = data.boards;
-        this.cols = data.cols;
-        this.categorys = data.categorys;
+        this.data = data;
 
         this.init();
     }
@@ -35,9 +30,14 @@ class Table {
     init(isReload = false) {
         if(isReload) {
             this.container.removeChild(document.getElementById(`booksTable`));
+            this.tableRef = undefined;
+            this.tableRef = document.createElement("table");
         }
         
         this.tableRef.setAttribute("id", `booksTable`);
+
+        this.boards = this.data.boards;
+        this.categorys = this.data.categorys;
 
         this.createHeader();
         this.createBody();
@@ -46,6 +46,8 @@ class Table {
     }
 
     createHeader() {
+        let trRef = this.tableRef.insertRow(-1);
+
         // 테이블 header set
         this.boards.forEach((board, idx) => {
             for (let key in this.boards[idx]) {
@@ -56,34 +58,33 @@ class Table {
         });
 
         this.cols.forEach((col) => {
-            this.trRef.innerHTML += `<th>${col}</th>`;
+            trRef.innerHTML += `<th>${col}</th>`;
         });
     }
 
     createBody() {
         for (let i = 0, cnt = this.boards.length; i < cnt; i++) {
-            this.trRef = this.tableRef.insertRow(-1);
+            let trRef = this.tableRef.insertRow(-1);
 
             for (let j = 0, cnt = this.cols.length; j < cnt; j++) {
-                let tabCell = this.trRef.insertCell(-1);
+                let tabCell = trRef.insertCell(-1);
                 tabCell.innerHTML = this.boards[i][this.cols[j]];
             }
 
-            this.tdRef = document.createElement("td");
+            let tdRef = document.createElement("td");
 
             // 취소
-            this.trRef.appendChild(this.tdRef);
+            trRef.appendChild(tdRef);
             let lblCancel = document.createElement("label");
             lblCancel.innerHTML = "✖";
-            lblCancel.setAttribute("onclick", "cancel()");
             lblCancel.setAttribute("style", "display:none;");
             lblCancel.setAttribute("title", "Cancel");
             lblCancel.setAttribute("id", "lbl" + i);
             lblCancel.addEventListener('click', this.cancel);
-            this.tdRef.appendChild(lblCancel);
+            tdRef.appendChild(lblCancel);
 
             // 저장
-            this.trRef.appendChild(this.tdRef);
+            trRef.appendChild(tdRef);
             let btSave = document.createElement("input");
 
             btSave.setAttribute("type", "button");
@@ -91,10 +92,10 @@ class Table {
             btSave.setAttribute("id", "Save" + i);
             btSave.setAttribute("style", "display:none;");
             btSave.addEventListener('click', this.save);
-            this.tdRef.appendChild(btSave);
+            trRef.appendChild(btSave);
 
             // 수정
-            this.trRef.appendChild(this.tdRef);
+            trRef.appendChild(tdRef);
             let btUpdate = document.createElement("input");
 
             btUpdate.setAttribute("type", "button");
@@ -102,17 +103,17 @@ class Table {
             btUpdate.setAttribute("id", "Edit" + i);
             btUpdate.setAttribute("style", "background-color:#44CCEB;");
             btUpdate.addEventListener('click', this.update);
-            this.tdRef.appendChild(btUpdate);
+            tdRef.appendChild(btUpdate);
 
             // 삭제
-            this.tdRef = document.createElement("td");
-            this.trRef.appendChild(this.tdRef);
+            tdRef = document.createElement("td");
+            trRef.appendChild(tdRef);
             let btDelete = document.createElement("input");
             btDelete.setAttribute("type", "button");
             btDelete.setAttribute("value", "Delete");
             btDelete.setAttribute("style", "background-color:#ED5650;");
             btDelete.addEventListener('click', this.remove);
-            this.tdRef.appendChild(btDelete);
+            tdRef.appendChild(btDelete);
         }
     }
 
@@ -135,43 +136,103 @@ class Table {
                     let tBox = document.createElement("input");
                     tBox.setAttribute("type", "text");
                     tBox.setAttribute("value", "");
-                    // tBox.onclick = this.createNew();
                     newRow.appendChild(tBox);
                 }
             }
         });
 
-        this.tdRef = document.createElement("td");
-        newTr.appendChild(this.tdRef);
+        let tdRef = document.createElement("td");
+        newTr.appendChild(tdRef);
 
         let btNew = document.createElement("input");
         btNew.setAttribute("type", "button");
         btNew.setAttribute("value", "Create");
-        // btNew.setAttribute("id", "New" + i);
         btNew.setAttribute("id", "New");
         btNew.setAttribute("style", "background-color:#207DD1;");
         btNew.addEventListener('click', this.create);
 
-        this.tdRef.appendChild(btNew);
+        tdRef.appendChild(btNew);
     }
 
     // 클릭 이벤트에 해당하는 this는 현재 input값을 가리킨다.
-    // DOM refresh는 어떻게하지...?
-
     cancel() {
         console.log("cancel");
+
+        this.setAttribute('style', 'display:none; float:none;');
+
+        let activeRow = this.parentNode.parentNode.rowIndex;
+
+        let btSave = document.getElementById('Save' + (activeRow - 1));
+        btSave.setAttribute('style', 'display:none;');
+
+        let btUpdate = document.getElementById('Edit' + (activeRow - 1));
+        btUpdate.setAttribute('style', 'display:block; margin:0 auto; background-color:#44CCEB;');
+
+        let tab = document.getElementById('booksTable').rows[activeRow];
+
+        table.cols.forEach((col, idx) => {
+            let td = tab.getElementsByTagName("td")[idx];
+            td.innerHTML = table.boards[(activeRow - 1)][col];
+        })
     }
     
     save() {
         console.log("save");
+
+        let activeRow = this.parentNode.rowIndex;
+        let tab = document.getElementById('booksTable').rows[activeRow];
+
+        table.cols.forEach((col, idx) => {
+            let td = tab.getElementsByTagName("td")[idx];
+
+            if(idx !== 0) {
+                if (td.childNodes[0].getAttribute('type') == 'text' || td.childNodes[0].tagName == 'SELECT') {
+                    data.boards[(activeRow - 1)][col] = td.childNodes[0].value; 
+                }
+            }
+        })
+
+        table.init(true);
     }
     
     update() {
         console.log("update");
+
+        let activeRow = this.parentNode.parentNode.rowIndex;
+        let tab = document.getElementById('booksTable').rows[activeRow];
+
+        for(let idx = 0; idx < 4; idx++) {
+            if (idx == 2) {
+                let td = tab.getElementsByTagName("td")[idx];
+                let ele = document.createElement('select');
+                ele.innerHTML = `<option value="${td.innerText}">${td.innerText}</option>'`;
+                data.categorys.forEach((categroy) => {
+                    ele.innerHTML = `${ele.innerHTML}<option value="${categroy}">${categroy}</option>'`;
+                })
+                td.innerText = '';
+                td.appendChild(ele);
+            }
+            else if(idx !== 0) {
+                let td = tab.getElementsByTagName("td")[idx];
+                let ele = document.createElement('input');
+                ele.setAttribute('type', 'text');
+                ele.setAttribute('value', td.innerText);
+                td.innerText = '';
+                td.appendChild(ele);
+            }
+        }
+
+        let lblCancel = document.getElementById('lbl' + (activeRow - 1));
+        lblCancel.setAttribute('style', 'cursor:pointer; display:block; width:20px; float:left; position: absolute;');
+
+        let btSave = document.getElementById('Save' + (activeRow - 1));
+        btSave.setAttribute('style', 'display:block; margin-left:30px; float:left; background-color:#2DBF64;');
+
+        this.setAttribute('style', 'display:none;');
     }
     
     remove() {
-        console.log("remove", this);
+        console.log("remove");
 
         let activeRow = this.parentNode.parentNode.rowIndex;
         data.boards.splice((activeRow - 1), 1);
@@ -180,6 +241,33 @@ class Table {
     
     create() {
         console.log("create");
+
+        let activeRow = this.parentNode.parentNode.rowIndex;
+        let tab = document.getElementById('booksTable').rows[activeRow];
+        let obj = {};
+
+        table.cols.forEach((col, idx) => {
+            let td = tab.getElementsByTagName("td")[idx];
+            
+            if(td.childNodes[0] !== undefined) {
+                if (td.childNodes[0].getAttribute('type') == 'text' || td.childNodes[0].tagName == 'SELECT') {
+                    let txtVal = td.childNodes[0].value;
+                        if (txtVal != '') {
+                            obj[col] = txtVal.trim();
+                        }
+                        else {
+                            obj = '';
+                            alert('입력하세요');
+                        }
+                }
+            }
+            else {
+                obj[col] = table.boards.length + 1;   
+            }
+        })
+
+        data.boards.push(obj);
+        table.init(true);
     }
 }
 
